93f22dc5c2accf5dcd4788a3dd1815821385e888
qryandia <qryandia@gmail.com>
2025-10-10 09:11:02 +0700

    landing page

--- resources/views/guest/daftarKos.blade.php
+++ resources/views/guest/daftarKos.blade.php
@@ -4,7 +4,141 @@
 
 @section('content')
 
+<!-- search -->
+<div class="p-12 mt-8 flex justify-between items-center px-10 py-6">
+    <div class="primjudul textprim">
+        Daftar Kos-kosan Sukorame
+    </div>
+
+    <div class="flex items-center gap-3">
+        <div class="flex items-center border border-gray-300 rounded-full px-4 py-2 w-80 shadow-sm">
+            <i class="fas fa-search text-gray-400 mr-2"></i>
+            <input
+                type="text"
+                placeholder="Cari nama kos di Kelurahan Sukorame"
+                class="outline-none w-full text-sm text-gray-700"
+            />
+        </div>
+
+        <button
+            class="flex items-center gap-2 border border-gray-300 rounded-xl px-4 py-2 text-gray-500 hover:bg-gray-100 transition">
+            <i class="fas fa-filter"></i>
+            <span>Filter</span>
+            <i class="fas fa-chevron-down text-sm"></i>
+        </button>
+    </div>
+</div>
+
+</div>
+<!-- search -->
 
 
+<div class="px-12 grid grid-cols-12 gap-3">
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Kos Bu Rahmat</h3>
+                <div class="tagcwe">
+                    Kos Perempuan
+                </div>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagcwo">Kos Laki-laki</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagbeb">Kos Bebas</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagbeb">Kos Bebas</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+</div>
+
+<!-- row bawah -->
+<div class="px-12 pt-12 mb-20 grid grid-cols-12 gap-3">
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Kos Bu Rahmat</h3>
+                <div class="tagcwo">
+                    Kos Perempuan
+                </div>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagcwo">Kos Laki-laki</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagcwe">Kos Bebas</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+    <div class="card col-span-3">
+        <img src="img/kos1.svg" alt="gbrkos">
+        <div class="card-content">
+            <div class="card-header">
+                <h3>Nama Kos</h3>
+                <span class="tagbeb">Kos Bebas</span>
+            </div>
+            <p class="location">
+                <i class="fas fa-map-marker-alt"></i> Jln. Sukorame...
+            </p>
+        </div>
+    </div>
+</div>
+<!-- klo dh make database noh -->
 
 @endsection
--- resources/views/guest/layouts/nav.blade.php
+++ resources/views/guest/layouts/nav.blade.php
@@ -7,7 +7,7 @@
         <li class="ml-8">
             <a href="{{ route('landingPage') }}"
                class="font-medium pb-0.5 border-b-2 transition-colors duration-300
-                      {{ Route::is('landingPage') ? 'text-blue-700 border-blue-700' : 'text-gray-500 hover:text-blue-700 border-transparent' }}">
+                      {{ Route::is('landingPage') ? 'primwar border-[#704E98]' : 'textmati cahaya border-transparent' }}">
                Beranda
             </a>
         </li>
@@ -15,7 +15,7 @@
         <li class="ml-8">
             <a href="{{ route('daftarKos') }}"
                class="font-medium pb-0.5 border-b-2 transition-colors duration-300
-                      {{ Route::is('daftarKos') ? 'text-blue-700 border-blue-700' : 'text-gray-500 hover:text-blue-700 border-transparent' }}">
+                      {{ Route::is('daftarKos') ? 'primwar border-[#704E98]' : 'textmati cahaya border-transparent' }}">
                Daftar Kos Aktif
             </a>
         </li>
@@ -23,7 +23,7 @@
         <li class="ml-8">
             <a href="{{ route('tentang') }}"
                class="font-medium pb-0.5 border-b-2 transition-colors duration-300
-                      {{ Route::is('tentang') ? 'text-blue-700 border-blue-700' : 'text-gray-500 hover:text-blue-700 border-transparent' }}">
+                      {{ Route::is('tentang') ? 'primwar border-[#704E98]' : 'textmati cahaya border-transparent' }}">
                Tentang
             </a>
         </li>
@@ -32,6 +32,6 @@
     <a href="{{ route('login') }}"
        class="font-medium py-3 px-6 rounded-full transition-colors duration-300
               {{ Route::is('login') ? 'bg-[#512D84] text-white' : 'bg-[#704E98] text-white hover:bg-[#512D84]' }}">
-       Masuk Admin
+       Masuk
     </a>
 </nav>
